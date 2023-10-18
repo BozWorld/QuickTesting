b322d031930bcc498b423db07ddcff25b467f153
BozWorld <mondohamoeva@hotmail.fr>
2023-10-18 10:22:59 +0200

    {{Trying Raycast}}

--- Scene_Manager.gd
+++ Scene_Manager.gd
@@ -4,4 +4,8 @@ func _ready():
 	$Player.mySignal.connect($HUD._receive_Velocity)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if($RayCastLearning/RayCast2D.is_colliding()):
+		
 	$HUD/FpsCounter.text = str(Engine.get_frames_per_second())
+
+
